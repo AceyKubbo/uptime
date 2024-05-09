e6ea556b4f517697c0fdeba41c3efb7214cc851a
Kubbo <390378816@qq.com>
2024-05-09 11:33:29 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,11 +1,10 @@
 const pageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "Kubbo Server Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.site/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.site', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/AceyKubbo', label: 'GitHub' },
+    { link: 'https://cdn.sinoecare.com', label: 'sinoecare' },
   ],
 }
 
